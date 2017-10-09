c016e81ddc009c3663b76ccee2d1168de0d35cfe
Karim Habashi <iammonkee@gmail.com>
2017-10-09 16:29:51 -0700

    finished populate method and standard error for move_pos method

--- board.rb
+++ board.rb
@@ -1,25 +1,33 @@
+require_relative 'piece.rb'
+
 class Board
 
   def initialize(pos = nil)
     @grid = Array.new(8) {Array.new(8)}
-
   end
 
   def populate
-    @grid.each_with_index do |row,idx|
-      row.each do |col|
-        if idx == 0 || idx == 1
+    (0..7).each do |row|
+      (0..7).each do |col|
+        if row == 0 || row == 1
           self[[row, col]] = Piece.new("White")
-        elsif idx == 6 || idx == 7
+        elsif row == 6 || row == 7
           @grid[row][col] = Piece.new("Black")
         else
           @grid[row][col] = NullPiece.new
+        end
       end
     end
   end
 
   def move_piece(start_pos, end_pos)
-
+    if self[start_pos].is_a?(NullPiece)
+      raise StandardError.new "There is no piece at start poistion"
+    elsif (end_pos[0] || end_pos[1] > 7) || (end_pos[0] || end_pos[1] < 0)
+      raise StandardError.new "The end position is not on the board"
+    elsif self[end_pos].color == self[start_pos].color
+      raise StandardError.new "There is already a piece at that position"
+    end
   end
 
   def [](pos)
@@ -31,5 +39,8 @@ class Board
     row, col = pos
     @grid[row][col] = val
   end
-
 end
+
+board = Board.new
+board.populate
+p board
